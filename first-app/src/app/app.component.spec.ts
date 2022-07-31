dbe1baa82aa1e8b5fb7b68d75f85972d6c6da20e
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-07-31 15:18:52 +0530

    getting ready for the project.

--- first-app/src/app/app.component.spec.ts
+++ first-app/src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'first-app'`, () => {
+  it(`should have as title 'anurag'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('first-app');
+    expect(app.name).toEqual('anurag');
   });
 
   it('should render title', () => {
